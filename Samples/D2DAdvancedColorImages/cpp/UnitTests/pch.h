2ee0b98945ab1cdbe297cd01d27514b0021de80e
Simon Tao <simon.tao@microsoft.com>
2019-02-26 00:18:41 -0800

    Trying to add unit tests...

---
+++ Samples/D2DAdvancedColorImages/cpp/UnitTests/pch.h
@@ -0,0 +1,18 @@
+﻿//
+// pch.h
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+// DirectX
+#include <dxgi1_6.h>
+#include <d3d11_3.h>
+#include <d2d1_3.h>
+#include <dwrite_3.h>
+#include <wincodec.h>
+#include <DirectXMath.h>
+
+#include "UnitTestApp.xaml.h"
