377d6e13a3a5a3f0eb2c88a8c33d9ec6ac8e8ade
Simon Tao <simon.tao@microsoft.com>
2019-02-28 00:13:46 -0800

    add (untested) DumpD2DTarget

--- Samples/D2DAdvancedColorImages/cpp/D2DAdvancedColorImages/ImageExporter.h
+++ Samples/D2DAdvancedColorImages/cpp/D2DAdvancedColorImages/ImageExporter.h
@@ -20,8 +20,10 @@ namespace D2DAdvancedColorImages
         ~ImageExporter();
 
         static void ExportToSdr(_In_ ImageLoader* loader, _In_ DX::DeviceResources* res, IStream* stream, GUID wicFormat);
-        static void ExportToWic(_In_ ID2D1Image* img, Windows::Foundation::Size size, _In_ DX::DeviceResources* res, IStream* stream, GUID wicFormat);
+
+        static std::vector<DirectX::XMFLOAT4> DumpD2DTarget(_In_ DX::DeviceResources* res);
 
     private:
+        static void ExportToWic(_In_ ID2D1Image* img, Windows::Foundation::Size size, _In_ DX::DeviceResources* res, IStream* stream, GUID wicFormat);
     };
 }
