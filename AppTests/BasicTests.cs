4bdb6626364b6b31b2ae5b759d35c8419a0cc87c
Simon Tao <13thsymphony@gmail.com>
2020-07-31 21:59:38 -0700

    Placeholder Appium project for CS app unit tests

---
+++ AppTests/BasicTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace AppTests
+{
+    [TestClass]
+    public class BasicTests : HdrImageViewerTestSession
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Create session to launch app window.
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void Clear()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            session.FindElementByXPath("/Button[@ClassName=\"Button\"][@Name=\"Open\"]").Click();
+        }
+    }
+}
---
+++ AppTests/TestSession.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace AppTests
+{
+    public class HdrImageViewerTestSession
+    {
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string AppId = "24700lepusmagnum.HDRandWCGImageViewer_35x0ett40cyc6!App";
+
+        protected static WindowsDriver<WindowsElement> session;
+
+            public static void Setup(TestContext context)
+        {
+            // Launch application if it is not yet launched
+            if (session == null)
+            {
+                // Create a new session to bring up an instance of the application.
+                // Note: Multiple app windows (instances) share the same process Id.
+                AppiumOptions appCapabilities = new AppiumOptions();
+                appCapabilities.AddAdditionalCapability("app", AppId);
+                appCapabilities.AddAdditionalCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+
+                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+            }
+        }
+
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
